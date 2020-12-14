c3b080e4ff1fe7f60d600a95e798956c65056f25
Sarunkorn <sirlight.k@gmail.com>
2020-12-14 23:45:47 +0700

    - add: search function

--- src/components/select_card_modal.tsx
+++ src/components/select_card_modal.tsx
@@ -7,6 +7,11 @@ import CardComponent from './card_component';
 
 const b = bem('SelectCardModal');
 
+enum SearchTypes {
+  Name = 'byName',
+  Type = 'byType',
+}
+
 export interface SelectCardProps {
   cards: CardModel[];
 }
@@ -14,23 +19,59 @@ export interface SelectCardProps {
 function SelectCardModal(props: SelectCardProps) {
   const { cards } = props;
   const [keywords, setKeywords] = useState('');
+  const [searchType, setSearchType] = useState(SearchTypes.Name);
   const myPokedex = useRecoilValue(myPokedexAtom);
   const idMap = new Set<string>(myPokedex.map((o) => o.id));
 
-  console.log(idMap);
-
-  const onChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onKeywordChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeywords(e.target.value);
   };
 
-  const filteredCards = keywords
-    ? cards.filter((o) => !idMap.has(o.id) && o.name.toLowerCase().startsWith(keywords.toLowerCase(), 0))
-    : cards.filter((o) => !idMap.has(o.id));
+  const onSearchTypeChanged = (type: SearchTypes) => {
+    setSearchType(type);
+  };
+
+  const searchMap = {
+    byName: (card: CardModel) => {
+      return !idMap.has(card.id) && card.name.toLowerCase().startsWith(keywords.toLowerCase(), 0);
+    },
+    byType: (card: CardModel) => {
+      return !idMap.has(card.id) && card.type.toLowerCase().startsWith(keywords.toLowerCase(), 0);
+    },
+  };
+
+  const filteredCards = keywords ? cards.filter(searchMap[searchType]) : cards.filter((o) => !idMap.has(o.id));
 
   return (
     <div className={b()}>
       <div className={b('search')}>
-        <input type="text" placeholder="Search.." name="search" onChange={onChanged} />
+        <input
+          className={b('search', ['input'])}
+          type="text"
+          placeholder="Search.."
+          name="search"
+          onChange={onKeywordChanged}
+        />
+        <div className={b('search', ['checkbox'])}>
+          <label>
+            <input
+              type="checkbox"
+              name="byName"
+              checked={searchType === SearchTypes.Name}
+              onChange={(e) => onSearchTypeChanged(SearchTypes.Name)}
+            />
+            By Name
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="byType"
+              checked={searchType === SearchTypes.Type}
+              onChange={(e) => onSearchTypeChanged(SearchTypes.Type)}
+            />
+            By Type
+          </label>
+        </div>
       </div>
       <div className={b('cards')}>
         {filteredCards.map((o, i) => {
