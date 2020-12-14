6e2fbf8d59e7da48f75022c790991990466dcb64
Sarunkorn <sirlight.k@gmail.com>
2020-12-14 18:50:15 +0700

    - add: card model

---
+++ src/model/card.ts
@@ -0,0 +1,42 @@
+export interface Ability {
+  name: string;
+  text: string;
+  type: string;
+}
+
+export interface Attack {
+  cost: string[];
+  name: string;
+  text: string;
+  damage: string;
+  convertedEnergyCost: number;
+}
+
+export interface Weakness {
+  type: string;
+  value: string;
+}
+
+export interface RootObject {
+  id: string;
+  name: string;
+  nationalPokedexNumber: number;
+  imageUrl: string;
+  imageUrlHiRes: string;
+  supertype: string;
+  subtype: string;
+  ability: Ability;
+  hp: string;
+  retreatCost: string[];
+  convertedRetreatCost: number;
+  number: string;
+  artist: string;
+  rarity: string;
+  series: string;
+  set: string;
+  setCode: string;
+  text: string[];
+  attacks: Attack[];
+  weaknesses: Weakness[];
+  type: string;
+}
