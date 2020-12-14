dfedda2295e104b3f33c6ba208e1e29aa96b3a98
Sarunkorn <sirlight.k@gmail.com>
2020-12-14 19:19:47 +0700

    - add: base http requests

---
+++ src/requests/generic_http_request.ts
@@ -0,0 +1,26 @@
+import { AxiosError } from "axios";
+import { BaseAPIConfig, BaseHttpRequest } from "./base_http_request";
+
+export type ConvertFunction<T> = (jsonObj: any) => jsonObj is T;
+
+export class GenericHTTPRequest<T> extends BaseHttpRequest<T> {
+  convertFunc: ConvertFunction<T>;
+
+  constructor(config: BaseAPIConfig, convertFunc: ConvertFunction<T>) {
+    super(config);
+    this.convertFunc = convertFunc;
+  }
+
+  protected convert(data: string): T {
+    const jsonObj = JSON.parse(data);
+    if (this.convertFunc(jsonObj)) {
+      return jsonObj as T;
+    } else {
+      throw "DATA_CONVERT_FAILED";
+    }
+  }
+
+  protected errorHandle(err: AxiosError<any>): never {
+    throw err;
+  }
+}
