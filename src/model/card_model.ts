ba3be267599f896ce312cc28d855e67f403f001d
Sarunkorn <sirlight.k@gmail.com>
2020-12-14 20:02:54 +0700

    - refacter: reduce card model parameter, remove unuse

--- src/model/card_model.ts
+++ src/model/card_model.ts
@@ -1,8 +1,4 @@
-export interface Ability {
-  name: string;
-  text: string;
-  type: string;
-}
+const TrainerType = 'Trainer';
 
 export interface Attack {
   cost: string[];
@@ -20,24 +16,12 @@ export interface Weakness {
 export interface CardModel {
   id: string;
   name: string;
-  nationalPokedexNumber: number;
   imageUrl: string;
   imageUrlHiRes: string;
   supertype: string;
-  subtype: string;
-  ability: Ability;
   hp: string;
-  retreatCost: string[];
-  convertedRetreatCost: number;
-  number: string;
-  artist: string;
-  rarity: string;
-  series: string;
-  set: string;
-  setCode: string;
-  text: string[];
-  attacks: Attack[];
-  weaknesses: Weakness[];
+  attacks?: Attack[];
+  weaknesses?: Weakness[];
   type: string;
 }
 
@@ -45,52 +29,18 @@ export interface CardModel {
 export function isCardModel(obj: unknown): obj is CardModel {
   const data = obj as CardModel;
 
-  const {
-    id,
-    name,
-    nationalPokedexNumber,
-    imageUrl,
-    imageUrlHiRes,
-    supertype,
-    subtype,
-    ability,
-    hp,
-    retreatCost,
-    convertedRetreatCost,
-    number,
-    artist,
-    rarity,
-    series,
-    set,
-    setCode,
-    text,
-    attacks,
-    weaknesses,
-    type,
-  } = data;
+  const { id, name, imageUrl, imageUrlHiRes, supertype, hp, attacks, weaknesses, type } = data;
 
   return (
     data &&
     id !== undefined &&
     name !== undefined &&
-    Number.isFinite(nationalPokedexNumber) &&
     imageUrl !== undefined &&
     imageUrlHiRes !== undefined &&
     supertype !== undefined &&
-    subtype !== undefined &&
-    ability !== undefined &&
     hp !== undefined &&
-    Array.isArray(retreatCost) &&
-    Number.isFinite(convertedRetreatCost) &&
-    number !== undefined &&
-    artist !== undefined &&
-    rarity !== undefined &&
-    series !== undefined &&
-    set !== undefined &&
-    setCode !== undefined &&
-    Array.isArray(text) &&
-    Array.isArray(attacks) &&
-    Array.isArray(weaknesses) &&
+    (attacks === undefined || Array.isArray(attacks)) &&
+    (weaknesses === undefined || Array.isArray(weaknesses)) &&
     type !== undefined
   );
 }
